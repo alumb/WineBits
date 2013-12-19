659cddeec56446b62b1892e74e5f4c4594b8bb3b
Curtis Lassam <curtis@lassam.net>
2013-12-18 22:16:15 -0800

    added winetruth frontpage

--- WineCore/truth/views.py
+++ WineCore/truth/views.py
@@ -87,6 +87,29 @@ class MyHandler(webapp2.RequestHandler):
             self.response.write( json.dumps(object_) )
 
 
+class BaseHandler(MyHandler):
+    """
+    / : returns a 'hello' sort of deal. 
+    """
+
+    def get(self):
+        self.response.write("""<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>WineBits: WineTruth</title>
+    <link rel="stylesheet" href="/style/css/bootstrap.min.css" />
+    <script src="/style/js/bootstrap.min.js"></script>
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,100,700,300' rel='stylesheet' type='text/css'>
+  </head>
+  <body>
+    <h1> Welcome to WineTruth. </h1>
+    <p> The documentation is available 
+    <a href='https://github.com/alumb/WineBits/blob/master/WineCore/truth/README.md'>here</a></p>
+  </body>
+</html>
+            """)
+
 class LocationHandler(MyHandler):
     """
     GET /location : an exhaustive list of every location
@@ -431,6 +454,7 @@ class SearchHandler(MyHandler):
 
 
 routes = [
+            (r'/', BaseHandler), 
             (r'/location/?', LocationHandler),
             (r'/winetype/?', WineTypeHandler),
             (r'/varietal/?', VarietalHandler),
