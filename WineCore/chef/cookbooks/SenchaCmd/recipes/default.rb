776a886f228b5bbc3e14043060f787c558cc0543
classam <curtis@lassam.net>
2014-02-06 16:07:31 -0800

    Vagrant returned to fully operational state with hax. And by HAX I mean telling the vagrantfile to install the right version of Java and using the 64-bit SenchaCmd binary rather than the 32-bit one.

--- WineCore/chef/cookbooks/SenchaCmd/recipes/default.rb
+++ WineCore/chef/cookbooks/SenchaCmd/recipes/default.rb
@@ -1,24 +1,23 @@
-#package "java" 
 package "unzip" 
 
 remote_file "/home/vagrant/sencha.zip" do
-    source "http://cdn.sencha.com/cmd/4.0.1.45/SenchaCmd-4.0.1.45-linux.run.zip"
+    source "http://cdn.sencha.com/cmd/4.0.2.67/SenchaCmd-4.0.2.67-linux-x64.run.zip"
     action :create_if_missing
 end
 
 execute "unzip sencha.zip" do
     cwd "/home/vagrant/"
     command "unzip sencha.zip" 
-    not_if { ::File.exists?("/home/vagrant/SenchaCmd-4.0.1.45-linux.run")}
+    not_if { ::File.exists?("/home/vagrant/SenchaCmd-4.0.2.67-linux-x64.run")}
 end
 
-file "/home/vagrant/SenchaCmd-4.0.1.45-linux.run" do
+file "/home/vagrant/SenchaCmd-4.0.2.67-linux-x64.run" do
   mode "0755"
 end
 
 execute "install SenchaCmd" do
     cwd "/home/vagrant/"
-    command "./SenchaCmd-4.0.1.45-linux.run --mode unattended --prefix /home/vagrant" 
+    command "./SenchaCmd-4.0.2.67-linux-x64.run --mode unattended --prefix /home/vagrant" 
     not_if { ::File.directory?("/home/vagrant/Sencha")}
 end
 
