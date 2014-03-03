750e8dc1bf6128b707b824a0f0218f7685bc29ab
Andy Lumb <andy.lumb@gmail.com>
2014-03-02 23:51:43 -0800

    Feature: adopted some of the WineBits colours. Added a title bar.

--- WineCore/cellar/PineSocks/app/view/Main.js
+++ WineCore/cellar/PineSocks/app/view/Main.js
@@ -1,20 +1,41 @@
 Ext.define('WineCellar.view.Main', {
-	extend: 'Ext.tab.Panel',
+	extend: 'Ext.panel.Panel',
 	xtype:"app-main",
 	requires:[
 		'Ext.tab.Panel',
 		'Ext.layout.container.Border',
 		'WineCellar.view.WineTruthList',
-		'WineCellar.view.Inventory'
+		'WineCellar.view.Inventory',
+		"Ext.toolbar.TextItem"
 	],
 	
 	layout:"fit",
-	activeTab:1,
+
+	dockedItems:[{
+		xtype:"toolbar",
+		cls:"TitleBar",
+		dock:"top",
+		items:[{
+			xtype:'tbtext',
+			cls:"title",
+			text:"WineBits - Cellar Admin"
+		},"->",{
+			xtype:'tbtext',
+			text:"Login"
+		}]
+	}],
+
+
 	items:[{
-		xtype:'WineTruthList',
-		title: 'Wine Truth List'
-	},{
-		xtype:'Inventory',
-		title: 'Inventory'
+		xtype:"tabpanel",
+
+		activeTab:1,
+		items:[{
+			xtype:'WineTruthList',
+			title: 'Wine Truth List'
+		},{
+			xtype:'Inventory',
+			title: 'Inventory'
+		}]
 	}]
 });
