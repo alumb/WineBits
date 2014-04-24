fab344c4c102d0887ebf3f3b267cda016ec876b0
Andy Lumb <andy.lumb@gmail.com>
2014-04-23 22:51:04 -0700

    Fixes to get sencha cmd working in vagrant
    few packages are missing from the default install.

--- WineCore/chef/cookbooks/SenchaCmd/recipes/default.rb
+++ WineCore/chef/cookbooks/SenchaCmd/recipes/default.rb
@@ -21,7 +21,17 @@ execute "install SenchaCmd" do
     not_if { ::File.directory?("/home/vagrant/Sencha")}
 end
 
+execute "install missing fontconfig package" do
+    cwd "/home/vagrant/"
+    command "sudo aptitude install fontconfig" 
+end
+
 execute "fixup /home/vagrant/Sencha permissions" do
   command "chown -Rf vagrant.vagrant /home/vagrant/Sencha"
   only_if { Etc.getpwuid(File.stat('/home/vagrant/Sencha').uid).name != "vagrant" }
+end
+
+execute "Add to path" do
+    cwd "/home/vagrant/"
+    command "export PATH=$PATH:/home/vagrant/Sencha/Cmd/4.0.2.67/" 
 end
